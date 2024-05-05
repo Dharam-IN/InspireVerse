f429491241922781ebf9755e323184432dfa6324
dharam-in <dharamdotin@gmail.com>
2024-05-05 14:51:00 +0530

    Versel File Add

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,9 @@
 import app from "./app.js";
 
+app.get("/", function(req, res){
+    res.send("Hello world")
+})
+
 app.listen(process.env.PORT, (req, res) =>{
     console.log(`Server Running on ${process.env.PORT} Port`)
 })
